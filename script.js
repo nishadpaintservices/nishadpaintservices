950840a358252940f0859477ee9898fa50ccd89e
nishadpaintservices <yash727542@gmail.com>
2025-09-06 14:59:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("quoteForm");
-const successMessage = document.getElementById("successMessage");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  successMessage.style.display = "block";
-  form.reset();
-});
